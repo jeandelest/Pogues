b2e755e302cc04263c7c5935624356202b1f09c3
Chloe Renaud <chloe.renaud@insee.fr>
2025-06-05 15:21:40 +0200

    fix: route tree gen

--- next/src/vite-env.d.ts
+++ next/src/vite-env.d.ts
@@ -8,6 +8,8 @@ export type ImportMetaEnv = {
   VITE_OIDC_ENABLED: string;
   VITE_OIDC_ISSUER: string;
   VITE_OIDC_CLIENT_ID: string;
+  VITE_AUTH_TYPE: string;
+  VITE_ENABLE_ALPHA_QUESTIONNAIRE_PAGE: string;
   BASE_URL: string;
   MODE: string;
   DEV: boolean;
