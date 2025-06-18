60696c4f80150084f9d5ec7357134f158df09ca7
Chloe Renaud <chloe.renaud@insee.fr>
2025-06-18 11:36:06 +0200

    build: fixed versions page env var

--- next/src/vite-env.d.ts
+++ next/src/vite-env.d.ts
@@ -9,7 +9,6 @@ export type ImportMetaEnv = {
   VITE_OIDC_ENABLED: string;
   VITE_OIDC_ISSUER: string;
   VITE_OIDC_CLIENT_ID: string;
-  VITE_AUTH_TYPE: string;
   VITE_ENABLE_VERSIONS_PAGE: string;
   BASE_URL: string;
   MODE: string;
