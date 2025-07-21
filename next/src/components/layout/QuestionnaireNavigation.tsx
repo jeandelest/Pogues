bd148bde69f80dcc9d6dbd21834715a3b2b85cbe
Mailine Nguyen <64129348+MailineN@users.noreply.github.com>
2025-07-21 07:37:42 +0200

    feat(next): personalization page (#1038)
    * feat: add questionnaire personalization feature
    
    ---------
    
    Co-authored-by: Chloe Renaud <chloe.renaud@insee.fr>

--- next/src/components/layout/QuestionnaireNavigation.tsx
+++ next/src/components/layout/QuestionnaireNavigation.tsx
@@ -8,6 +8,7 @@ import HistoryIcon from '@/components/ui/icons/HistoryIcon';
 import LatestIcon from '@/components/ui/icons/LatestIcon';
 import ListIcon from '@/components/ui/icons/ListIcon';
 import NomenclatureAltIcon from '@/components/ui/icons/NomenclatureAltIcon';
+import PersonalizeIcon from '@/components/ui/icons/PersonalizeIcon';
 import VariableIcon from '@/components/ui/icons/VariableIcon';
 import { useAltIcon } from '@/hooks/useAltIcon';
 
@@ -80,6 +81,12 @@ export default function QuestionnaireNavigation() {
       label: t('history.title'),
       Icon: HistoryIcon,
       path: '/questionnaire/$questionnaireId/versions',
+    },
+    {
+      label: t('personalization.title'),
+      Icon: PersonalizeIcon,
+      path: '/questionnaire/$questionnaireId/personalization',
+      innerPaths: ['/questionnaire/$questionnaireId/personalization/new'],
     },
   ];
 
